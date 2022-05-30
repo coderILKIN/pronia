89e32388baddf281fe7592b22fbd664ec7945f18
coderILKIN <tu758403c@code.edu.az>
2022-05-30 13:04:29 +0400

    add

---
+++ Pronia_task/Pronia_task/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia_task.Models;
+
+namespace Pronia_task.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
---
+++ Pronia_task/Pronia_task/Models/Slider.cs
@@ -0,0 +1,14 @@
+﻿namespace Pronia_task.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Subtitle { get; set; }
+        public string Title { get; set; }
+        public byte Discount { get; set; }
+        public string DiscoverUrl { get; set; }
+
+
+    }
+}
